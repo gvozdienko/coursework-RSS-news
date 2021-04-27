3bb6a9a87cbe5d37ea487f1c8354954c282b804e
gvozdienko <gvozdienko1234@gmail.com>
2021-04-27 11:48:45 +0300

    RSS-news: -collecting titles

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,41 @@
 package com.company;
-
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.net.*;
 public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        System.out.println(readRSS("https://www.0629.com.ua/rss"));
 
-    public static void main(String[] args) {
-	// write your code here
+    }
+ 
+    public static String readRSS(String urlAddress) throws IOException {
+        try {
+            URL rssUr1 = new URL(urlAddress);
+            BufferedReader in = new BufferedReader(new InputStreamReader(rssUr1.openStream()));
+            String sourceCode = "";
+            String line;
+            while ((line = in.readLine()) != null) {
+                if (line.contains("<title>")) {
+                    int firstPos = line.indexOf("<title>");
+                    String temp = line.substring(firstPos);
+                    temp = temp.replace("<title>", "");
+                    temp = temp.replace("<![CDATA[","");
+                    temp = temp.replace("]]>","");
+                    int lastPos = temp.indexOf("</title>");
+                    temp = temp.substring(0, lastPos);
+                    sourceCode += temp + "\n";
+
+                }
+            }
+            in.close();
+            return sourceCode;
+        } catch (MalformedURLException ue) {
+            System.out.println("Malformed URL");
+        } catch (IOException ioe) {
+            System.out.println("Something went wrong reading the contents");
+        }
+        return null;
     }
 }
